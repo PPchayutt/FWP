a596e98ffae45af46b25544cb0ba43d727db63e0
PPchayutt <67070115@kmitl.ac.th>
2026-01-26 02:49:14 +0700

    add lab6-2 and lab6-3

--- lab6/2/script.js
+++ lab6/2/script.js
@@ -1,34 +1,26 @@
-function showStudentScores(students) {
-    const container = document.getElementById('card-container');
-    container.innerHTML = '';
-    students.forEach(student => {
-        const card = document.createElement('div');
-        card.className = 'card';
+function createCards(students) {
+    const container = document.getElementById("card-container");
+    container.innerHTML = "";
 
-        let imgSrc = '';
-        if (student.gender === 'Male') {
-            imgSrc = 'img/img_male.png';
+    students.forEach((student, index) => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        let imgSrc = "";
+        if (student.gender === "Male") {
+            imgSrc = "img/img_male.png";
         } else {
-            imgSrc = 'img/img_female.png';
+            imgSrc = "img/img_female.png";
         }
 
         card.innerHTML = `
             <img src="${imgSrc}" alt="Profile" class="profile-img">
-            <h3>${student.name}</h3>
+            <div class="student-name">${index + 1}. ${student.name}</div>
             
-            <div class="score-box">
-                <div class="score-item">
-                    <span>Physics</span>
-                    <span class="score-val">${student.physics}</span>
-                </div>
-                <div class="score-item">
-                    <span>Maths</span>
-                    <span class="score-val">${student.maths}</span>
-                </div>
-                <div class="score-item">
-                    <span>English</span>
-                    <span class="score-val">${student.english}</span>
-                </div>
+            <div class="score-list">
+                <div>Physics : <span>${student.physics}</span></div>
+                <div>Mathmatics : <span>${student.maths}</span></div>
+                <div>English : <span>${student.english}</span></div>
             </div>
         `;
 
@@ -38,5 +30,5 @@ function showStudentScores(students) {
 
 fetch("student-score.json")
     .then(response => response.json())
-    .then(data => showStudentScores(data))
-    .catch(error => console.log('error', error));
+    .then(data => createCards(data))
+    .catch(error => console.log("Error:", error));
--- lab6/3/script.js
+++ lab6/3/script.js
@@ -1,24 +1,37 @@
-function showData(employees) {
-    const tableBody = document.getElementById("employee-tbody");
-    tableBody.innerHTML = "";
+let allQuestions = [];
 
-    employees.forEach(emp => {
-        const row = document.createElement("tr");
-        const fullName = `${emp.FirstName} ${emp.LastName}`;
-        const genderShort = emp.Gender.charAt(0);
-
-        row.innerHTML = `
-            <td>${emp.id}</td>
-            <td>${fullName}</td>
-            <td>${genderShort}</td>
-            <td>${emp.Position}</td>
-            <td>${emp.Address}</td>
-        `;
-
-        tableBody.appendChild(row);
-    });
-}
-fetch("employees.json")
+fetch("questionAnswerData.json")
     .then(response => response.json())
-    .then(data => showData(data))
+    .then(data => {
+        allQuestions = data;
+        displayQuiz(data);
+    })
     .catch(error => console.log("error", error));
+
+function displayQuiz(questions) {
+    const container = document.getElementById("quiz-container");
+    let html = "";
+
+    questions.forEach((item, index) => {
+        let answersHtml = "";
+        for (let key in item.answers) {
+            if (key !== "correct") {
+                answersHtml += `
+                    <label class="answer-option">
+                        <input type="radio" name="question${index}" value="${key}">
+                        ${key.toUpperCase()}. ${item.answers[key]}
+                    </label>
+                `;
+            }
+        }
+
+        html += `
+            <div class="question-box">
+                <div class="question-text">${index + 1}. ${item.question}</div>
+                ${answersHtml}
+            </div>
+        `;
+    });
+
+    container.innerHTML = html;
+}
