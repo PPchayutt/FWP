6d91fdc966d0a2e1a281a6a734babbd315d61c7c
PPchayutt <67070115@kmitl.ac.th>
2026-02-05 23:30:17 +0700

    add lab7-2

--- lab7/2/index.js
+++ lab7/2/index.js
@@ -9,8 +9,28 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/home.html'));
 });
 
-app.get('/about', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/about.html'));
+app.get('/food1', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/food1.html'));
+});
+
+app.get('/food2', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/food2.html'));
+});
+
+app.get('/food3', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/food3.html'));
+});
+
+app.get('/food4', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/food4.html'));
+});
+
+app.get('/food5', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/food5.html'));
+});
+
+app.get('/food6', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/food6.html'));
 });
 
 app.listen(port, () => {
